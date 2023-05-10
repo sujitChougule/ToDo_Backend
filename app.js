19c7a6b11cb235596fdd059318421701ab1adae3
sujitChougule <sujitchougule2003@gmail.com>
2023-05-11 02:41:07 +0530

    may be cors problem solved

--- app.js
+++ app.js
@@ -1,39 +1,35 @@
 import express from "express";
 import userRouter from "./routes/user.js";
 import taskRouter from "./routes/task.js";
-import cors from 'cors';
+import cors from "cors";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
 
-
-
-export const app= express();
+export const app = express();
 //using middleware must use before routes
 app.use(express.json());
 //cookie parser
 app.use(cookieParser());
 
-app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods:["GET","POST","PUT","DELETE"],
-    Credentials:true, 
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 //using routes
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/task",taskRouter);
-
-
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
 
 config({
-    path:"./data/config.env"
-})
+  path: "./data/config.env",
+});
 
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
-app.get("/",(req,res)=>{
-    res.send("working")
-})
-
-app.use(errorMiddleware)
-
+app.use(errorMiddleware);
